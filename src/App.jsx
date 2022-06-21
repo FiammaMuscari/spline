b8d193b58bc442df8bad02641a421f4ead2d6782
Fiamma Muscari <fiammamuscari@gmail.com>
2022-06-21 03:50:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Spline from '@splinetool/react-spline';
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="app">
+        <Spline scene="https://prod.spline.design/BE14baOLZNSSZjyz/scene.splinecode" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Scene.jsx
@@ -0,0 +1,7 @@
+// import Spline from '@splinetool/react-spline';
+
+// export default function Scene() {
+//   return (
+//     <Spline scene="https://prod.spline.design/BE14baOLZNSSZjyz/scene.splinecode" />
+//   );
+// }
